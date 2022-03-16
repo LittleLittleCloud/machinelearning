b4d6e37ebd48314d59d658009ef5f78ae109dc54
XiaoYun Zhang <xiaoyuz@microsoft.com>
2022-03-16 11:31:09 -0700

    add comments

--- src/Microsoft.ML.SearchSpace/Tuner/ITuner.cs
+++ src/Microsoft.ML.SearchSpace/Tuner/ITuner.cs
@@ -2,16 +2,21 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace Microsoft.ML.SearchSpace
 {
-    internal interface ITuner
+    /// <summary>
+    /// interface for all tuners.
+    /// </summary>
+    public interface ITuner
     {
+        /// <summary>
+        /// Propose the next <see cref="Parameter"/> from current <paramref name="searchSpace"/>.
+        /// </summary>
         Parameter Propose(SearchSpace searchSpace);
 
+        /// <summary>
+        /// update tuner using according training result. This can only apply to smart tuners which needs training result to adjust next proposed <see cref="Parameter"/> from <see cref="Propose(SearchSpace)"/>
+        /// </summary>
         void Update(Parameter param, double metric, bool isMaximize);
     }
 }
